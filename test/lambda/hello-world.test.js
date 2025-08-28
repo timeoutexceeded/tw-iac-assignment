53ac2e129f68069556e044e6200ee24a96fed962
Bhanu Teja <bhanutv@thoughtworks.com>
2025-08-28 15:23:09 +0530

    Add hello world lambda

---
+++ test/lambda/hello-world.test.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+
+test("GET /hello should return Hello World", async () => {
+     const url = process.env.API_URL + "/hello";
+
+     const res = await fetch(url);
+     const data = res.json();
+     
+     expect(res.status).toBe(200);
+     expect(data.message).toBe("Hello, world!");
+});
